5f830e32a4c5665ff0b1a3fb98a1b88d75d4acdf
ZacharyHorine <zachary@horine.dev>
2020-12-04 21:38:00 -0800

    Day 3 - Part 2

--- day3.js
+++ day3.js
@@ -1,17 +1,39 @@
 part1();
+part2();
 
 function part1() {
   let lines = readFile("./day3_input.txt");
+  let trees = treeCount(lines,3,1);
+  console.log("Trees: " + trees);
+}
+
+function part2() {
+  let lines = readFile("./day3_input.txt");
+  let trees = treeCount(lines,1,1);
+  trees *= treeCount(lines,3,1);
+  trees *= treeCount(lines,5,1);
+  trees *= treeCount(lines,7,1);
+  trees *= treeCount(lines,1,2);
+  console.log("Trees: " + trees);
+}
+
+function treeCount(lines,xSlope,ySlope) {
   let treecount = 0;
   let xpos = 0;
-  for (line of lines) {
-    if (line[xpos] == "#") {
-      treecount++;
+  for (let y = 0; y < lines.length; y += ySlope) {
+    if (y > lines.length) {
+      //ran off the end of the array
+    } else {
+      let line = lines[y];
+      if (line[xpos] == "#") {
+        treecount++;
+      }
+      xpos = (xpos + xSlope)%(line.length);
     }
-    xpos = (xpos + 3)%(line.length);
   }
-  console.log("Trees: " + treecount);
+  return treecount;
 }
+
 
 function readFile(filename) {
   const fs = require('fs');
