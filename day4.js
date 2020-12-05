e6879432142a7607c3b697935777da56119dc6f5
ZacharyHorine <zachary@horine.dev>
2020-12-05 01:34:50 -0800

    Day 4 - Part 2

--- day4.js
+++ day4.js
@@ -1,6 +1,6 @@
 
 part1();
-
+part2();
 
 function part1() {
   let lines = readFile("./day4_input.txt");
@@ -17,6 +17,30 @@ function part1() {
       for (key of keys) {
         let header = key.trim().split(":")[0];
         currentPassport.push(header);
+      }
+    }
+  }
+  console.log(validPassports);
+}
+
+function part2() {
+  let lines = readFile("./day4_input.txt");
+  let validPassports = 0;
+  let currentPassport = [];
+  for (line of lines) {
+    if (line == "") {
+      if (checkValid(currentPassport)) {
+        validPassports++;
+      }
+      currentPassport = [];
+    } else {
+      let keys = line.match(/(?:[\s]|^)?(byr:(19[2-9][0-9]|200[0-2])|iyr:20(1[0-9]|20)|eyr:20(2[0-9]|30)|hgt:(1[5-8][0-9]cm|19[0-3]cm|[5-6][0-9]in|7[0-6]in)|hcl:#[0-9a-f]{6}|ecl:(amb|blu|brn|gry|grn|hzl|oth)|pid:[0-9]{9}(?:[\s]|$))/g);
+      console.log(keys);
+      if (keys) {
+        for (key of keys) {
+          let header = key.trim().split(":")[0];
+          currentPassport.push(header);
+        }
       }
     }
   }
