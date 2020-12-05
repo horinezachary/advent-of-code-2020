2f42a1d6f529c88fecc691d09300402487c03e77
ZacharyHorine <zachary@horine.dev>
2020-12-05 03:38:25 -0800

    Day 5 - Part 2

--- day5.js
+++ day5.js
@@ -1,4 +1,5 @@
 part1();
+part2();
 //testPasses();
 
 function part1() {
@@ -6,12 +7,29 @@ function part1() {
   let maxSeat = 0;
   for (line of lines) {
     let ret = consumeInput(line,0,127,0,7)
-    console.log(ret.seatId);
     if (ret.seatId > maxSeat) {
       maxSeat = ret.seatId;
     }
   }
   console.log("MAX SEAT ID: " + maxSeat);
+}
+
+function part2() {
+  let lines = readFile("./day5_input.txt");
+  let seats = [];
+  for (line of lines) {
+    let ret = consumeInput(line,0,127,0,7)
+    seats.push(ret);
+  }
+  seats.sort(function(a,b){return a.seatId - b.seatId});
+  for (i in seats) {
+    if (i != 0 && i != seats.length) {
+      if (seats[i].seatId - 1 != seats[i-1].seatId) {
+        console.log("Seat " + (seats[i].seatId - 1) + " is empty");
+        console.log(seats[i]);
+      }
+    }
+  }
 }
 
 function testPasses() {
@@ -40,7 +58,7 @@ function consumeInput(string, minRow, maxRow, minCol, maxCol) {
     minCol = minCol + Math.floor((maxCol-minCol+1)/2);
     ret = consumeInput(string.substring(1), minRow, maxRow, minCol, maxCol);
   }
-  console.log(string + ":" + "{" + minRow + "," + maxRow + "," + minCol + "," + maxCol + "}");
+  //console.log(string + ":" + "{" + minRow + "," + maxRow + "," + minCol + "," + maxCol + "}");
   if (string == "") {
     let seatId = (minRow * 8) + minCol;
     return {"row" : minRow, "col": minCol, "seatId": seatId};
