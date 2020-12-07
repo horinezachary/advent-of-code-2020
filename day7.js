8c931f1f038e284029c17f7724e318e8586c331f
ZacharyHorine <zachary@horine.dev>
2020-12-06 22:28:58 -0800

    Day 7 - Part 2

--- day7.js
+++ day7.js
@@ -1,4 +1,5 @@
 part1();
+part2();
 
 function part1() {
   let lines = readFile("./day7_input.txt");
@@ -18,6 +19,28 @@ function part1() {
   }
   let bagList = containsBag(bags,"shiny gold bag");
   console.log(bagList.length);
+}
+
+function part2() {
+  let lines = readFile("./day7_input.txt");
+  let bags = [];
+  for (line of lines) {
+    let holder = line.substring(0,line.indexOf("s contain "));
+    console.log(line);
+    let contents = line.match(/([0-9]+ [a-z ]*)(?:bag)/g);
+    if (!contents) {
+      contents = [];
+    }
+    for (i in contents) {
+      let number = contents[i].substring(0,contents[i].search(/[a-z]/)).trim();
+      let name = contents[i].substring(contents[i].search(/[a-z]/));
+      contents[i] = {name,number};
+    }
+    console.log(contents);
+    bags.push({holder,contents})
+  }
+  let containedBags = countBags(bags,"shiny gold bag") -1;
+  console.log(containedBags);
 }
 
 function containsBag(bags, target) {
@@ -40,6 +63,22 @@ function containsBag(bags, target) {
     }
   }
   return bagList;
+}
+
+function countBags(bags,target) {
+  console.log("TARGET: " + target);
+  let count = 0;
+  for (bag of bags) {
+    let baggyboi = bag;
+    if (baggyboi.holder == target) {
+      console.log(baggyboi.contents);
+      for (innerBag of baggyboi.contents) {
+        count += (innerBag.number * countBags(bags,innerBag.name));
+      }
+    }
+  }
+  console.log("Count: " + count);
+  return count+1;
 }
 
 function contains(contents,target) {
